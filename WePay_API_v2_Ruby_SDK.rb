72471c964f57520d45e64c00187a5af8de5aedd3
LeBlanc <andrew@everribbon.com>
2011-12-14 12:16:04 -0800

    fixing the endpoints so that the correct endpoints are used for API calls and the OAuth2 Authorization screen

--- WePay_API_v2_Ruby_SDK.rb
+++ WePay_API_v2_Ruby_SDK.rb
@@ -10,12 +10,24 @@ helps you make API calls to the WePay API v2
 =end
 
 class WePay
+	
+	STAGE_API_ENDPOINT = "https://stage.wepayapi.com/v2"
+	STAGE_UI_ENDPOINT = "https://stage.wepay.com/v2"
+	
+	PRODUCTION_API_ENDPOINT = "https://wepayapi.com/v2"
+	PRODUCTION_UI_ENDPOINT = "https://www.wepay.com/v2"
 		
 	# initializes the API application, api_endpoint should be something like 'https://stage.wepay.com/v2'
-	def initialize(_client_id, _client_secret, _api_endpoint = 'https://stage.wepay.com/v2', _use_ssl = true)
+	def initialize(_client_id, _client_secret, _use_stage = true, _use_ssl = true)
 		@client_id = _client_id
 		@client_secret = _client_secret
-		@api_endpoint = _api_endpoint
+		if _use_stage
+			@api_endpoint = STAGE_API_ENDPOINT
+			@ui_endpoint = STAGE_UI_ENDPOINT
+		else
+			@api_endpoint = PRODUCTION_API_ENDPOINT
+			@ui_endpoint = PRODUCTION_UI_ENDPOINT
+		end
 		@use_ssl = _use_ssl
 	end
 	
@@ -23,6 +35,7 @@ class WePay
 	def call(call, access_token = false, params = false)
 		# get the url
 		url = URI.parse(@api_endpoint + call)
+		p url
 		# construct the call data and access token
 		call = Net::HTTP::Post.new(url.path, initheader = {'Content-Type' =>'application/json'})
 		if params
@@ -42,8 +55,8 @@ class WePay
 	
 	# this function returns the URL that you send the user to to authorize your API application
 	# the redirect_uri must be a full uri (ex https://www.wepay.com)
-	def oauth2_authorize_url(redirect_uri, user_email = false, user_name = false)
-		url = @api_endpoint + '/oauth2/authorize?client_id=' + @client_id + '&redirect_uri=' + redirect_uri + '&scope=manage_accounts,view_balance,collect_payments,refund_payments,view_user' #scope is hard coded for now
+	def oauth2_authorize_url(redirect_uri, user_email = false, user_name = false, permissions = "manage_accounts,view_balance,collect_payments,refund_payments,view_user")
+		url = @ui_endpoint + '/oauth2/authorize?client_id=' + @client_id.to_s + '&redirect_uri=' + redirect_uri + '&scope=' + permissions
 		url += user_name ? '&user_name=' + CGI::escape(user_name) : ''
 		url += user_email ? '&user_email=' + CGI::escape(user_email) : ''
 	end
